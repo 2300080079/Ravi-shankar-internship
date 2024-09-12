b5566c4a8d8aba594416c43d8811f5cee0222022
Ravi Shankar Reddy <162668664+2300080079@users.noreply.github.com>
2024-09-12 09:54:04 +0530

    Add files via upload

---
+++ python development internship/DAY1 Q2.py
@@ -0,0 +1,7 @@
+#create variables to store your name, age, and favorite hobby. print these variables
+name = "ravi"
+age = 20
+hobby = "coding"
+print("name:", name)
+print("age:",age)
+print("favorite hobby:", hobby)
---
+++ python development internship/intenship/day 1/DAY1 Q1.py
@@ -0,0 +1,2 @@
+#write a python program that prints "hello world!" to the console.
+print("hello, world!")
